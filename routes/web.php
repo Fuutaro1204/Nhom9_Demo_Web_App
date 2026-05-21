98982bc294c9a8d806370d76023338a9945de38a
Fuutaro <fuutaro1204@gmail.com>
2026-05-21 10:31:09 +0700

    Add demo routes for login, register, dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,43 @@
+<!-- <?php
+
+        use App\Http\Controllers\ProfileController;
+        use Illuminate\Support\Facades\Route;
+
+        Route::get('/', function () {
+            return view('welcome');
+        });
+
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->middleware(['auth', 'verified'])->name('dashboard');
+
+        Route::middleware('auth')->group(function () {
+            Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+            Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+            Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        });
+
+        require __DIR__ . '/auth.php';
+        ?> -->
+
+
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+//use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// Demo cho báo cáo
+Route::get('/login', function () {
+    return view('auth.login');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+Route::get('/dashboard', function () {
+    return "Đây là trang Dashboard - Demo Bảo mật Web";
+});
